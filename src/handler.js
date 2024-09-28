4184170b68f45362784aa45e739e756bcc75d8f5
raisrasyiid <raisrasyiid01@gmail.com>
2024-09-28 20:32:51 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,91 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+// addBookHandler
+const addBookHandler = (request, h) => {
+    const {
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading,
+    } = request.payload;
+
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+    const finished = pageCount === readPage ? true : false;
+
+    const newBook = {
+        id,
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        finished,
+        reading,
+        insertedAt,
+        updatedAt,
+    };
+
+    if (readPage > pageCount) {
+        const response = h.response({
+            status: "fail",
+            message:
+                "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
+        });
+        response.code(400);
+        return response;
+    }
+
+    // Validasi jika nama kosong
+    if (!name) {
+        const response = h.response({
+            status: "fail",
+            message: "Gagal menambahkan buku. Mohon isi nama buku",
+        });
+        response.code(400);
+        return response;
+    }
+
+    // Push newBook ke array books
+    books.push(newBook);
+
+    const isSuccess = books.some((book) => book.id === id);
+
+    if (isSuccess) {
+        const response = h.response({
+            status: "success",
+            message: "Buku berhasil ditambahkan",
+            data: {
+                bookId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: "error",
+        message: "Buku gagal ditambahkan",
+    });
+    response.code(500);
+    return response;
+};
+
+const getBooksHandler = (request, h) => {
+    return h.response({
+        status: 'success',
+        data: {
+            books,
+        },
+    }).code(200);
+};
+
+module.exports = { addBookHandler, getBooksHandler };
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const { addBookHandler, getBooksHandler } = require('./handler');
+
+const routes = [
+    {
+        method: "POST",
+        path: "/books",
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getBooksHandler,
+    },
+];
+
+module.exports = routes;
